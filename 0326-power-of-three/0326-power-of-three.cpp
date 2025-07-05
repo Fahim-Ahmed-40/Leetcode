14a0e736130f1730946ccc34322826cc24f9bb93
Fahim-Ahmed-40 <fahimiit12@gmail.com>
2025-07-05 12:23:38 +0600

    Time: 3 ms (59.98%), Space: 9.1 MB (14.35%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         double ans;
-        if(n==0) return false;
+        if(n<=0) return false;
         for(int i=0;i<=31;i++){
             ans=pow(3,i);
             if(ans==n) return true;
