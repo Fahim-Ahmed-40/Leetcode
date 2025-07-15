43101e66fbdd0bef3ca0507d1d39a280497058b4
Fahim-Ahmed-40 <fahimiit12@gmail.com>
2025-07-15 12:21:09 +0600

    Time: 56 ms (32.86%), Space: 90.7 MB (71.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int>s(nums.begin(),nums.end());
-        vector<int>New_nums(s.begin(),s.end());
-        sort(nums.begin(),nums.end());
-        if(nums!=New_nums) return true;
-        else return false;
+        unordered_map<int,int>mapp;
+        for(int num:nums){
+            if(mapp[num]>=1)return true;
+            mapp[num]++;
+        }
+        return false;
     }
 };
